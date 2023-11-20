783591478e867394f22503190de2e40ac20d85cd
Yug Patel <113626506+pateyu@users.noreply.github.com>
2023-11-19 21:45:07 -0600

    Update main.c

--- main.c
+++ main.c
@@ -8,62 +8,74 @@
 
 #include <xc.h>
 #include <avr/io.h>
-
-
 #include <util/delay.h>
 
-#define DELAY_MS 1000 
+#define DELAY_MS 1000
+#define BUZZER_PIN PE4 
 
 void delay_ms(unsigned int ms) {
-	while(ms--) {
-		_delay_ms(1);
-	}
+    while(ms--) {
+        _delay_ms(1);
+    }
 }
 
 int main(void) {
-	
-	DDRD = 0b00011111;
-	
+    DDRD = 0b00011111; // Set PD0 to PD4 as output for counter
+    DDRE |= (1 << BUZZER_PIN); // Set PE4 as output for the buzzer
+    PORTE &= ~(1 << BUZZER_PIN); // Initially turn off the buzzer
 
-	
-	uint8_t counter = 0;
-	PORTD = 0xFF;  
+    PORTD = 0xFF;
 
-	
-	DDRA &= ~((1 << PA0) | (1 << PA2));
-	PORTA |= (1 << PA0) | (1 << PA2);
+    DDRA &= ~((1 << PA0) | (1 << PA1) | (1 << PA2)); // Configure PA0, PA1, and PA2 as input
+    PORTA |= (1 << PA0) | (1 << PA1) | (1 << PA2); // Enable pull-up resistors
 
-	while (1) {
-		
-		if (!(PINA & (1 << PA0))) {
-			
-			counter++;
+    uint8_t counter = 0;
+    
+    while (1) {
+        if (!(PINA & (1 << PA0))) {
+            counter++;
+            PORTD = ~counter;
 
-		
-			PORTD = ~counter;
+            if (counter > 31) {
+                PORTE |= (1 << BUZZER_PIN); // Turn on buzzer
+                delay_ms(DELAY_MS);
+                PORTE &= ~(1 << BUZZER_PIN); // Turn off buzzer
+                counter = 0; // Reset counter if it exceeds 31
+            }
 
-		
-			delay_ms(DELAY_MS);
+            delay_ms(DELAY_MS);
+            while(!(PINA & (1 << PA0))) {
+                delay_ms(DELAY_MS);
+            }
+        }
 
-			while(!(PINA & (1 << PA0))) {
-				delay_ms(DELAY_MS); 
-			}
-		}
+        if (!(PINA & (1 << PA1))) {
+            counter--;
+            PORTD = ~counter;
 
-		if (!(PINA & (1 << PA2))) {
-			counter--;
+            if (counter == 255) { // Underflow condition (counter < 0)
+                PORTE |= (1 << BUZZER_PIN); // Turn on buzzer
+                delay_ms(DELAY_MS);
+                PORTE &= ~(1 << BUZZER_PIN); // Turn off buzzer
+                counter = 31; // Reset counter if it goes below 0
+            }
 
-			PORTD = ~counter;
+            delay_ms(DELAY_MS);
+            while(!(PINA & (1 << PA1))) {
+                delay_ms(DELAY_MS);
+            }
+        }
 
-	
-			delay_ms(DELAY_MS);
+        if (!(PINA & (1 << PA2))) { // Clear board button pressed
+            counter = 0; // Reset the counter
+            PORTD = ~counter; // Update the display
 
-		
-			while(!(PINA & (1 << PA2))) {
-				delay_ms(DELAY_MS); 
-			}
-		}
-	}
+            delay_ms(DELAY_MS);
+            while(!(PINA & (1 << PA2))) {
+                delay_ms(DELAY_MS);
+            }
+        }
+    }
 
-	return 0;
+    return 0;
 }
